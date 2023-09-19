ad4736e33637ab00397ffc4342cf5e0649294826
Chebel405 <anne.chebel@gmail.com>
2023-09-19 12:11:22 +0200

    Affichage de la data

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -3,6 +3,16 @@ import React, { Component } from "react";
 export default class MovieDetails extends Component {
 
     render(){
-        return <div className="w-25 border p-4 p-2">MovieDetails</div>
+        return (
+            <div className="w-25 border p-4 d-flex flex-column">
+                <h5>{ this.props.movie.title}</h5>
+                <hr className="w-100" /> 
+                <div>
+                    <img className="d-block mx-auto"src={this.props.movie.img}/>
+                </div>
+                <span className="text-secondary">{this.props.movie.details}</span>
+                <p>{ this.props.movie.descriptions}</p>
+            </div>
+        )
     }
 }
