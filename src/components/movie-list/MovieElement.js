a98b0fa29f95b379d7b17b7a80933c999097350d
Chebel405 <anne.chebel@gmail.com>
2023-09-19 16:09:36 +0200

    Fichier Loading

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -10,16 +10,16 @@ export default class MovieElement extends Component {
   render() {
 
     return (
-      <div onMouseEnter={this.mouseEnter}className="w-25 p-2">
+      <div onMouseEnter={ this.mouseEnter } className="w-50 p-2">
         <div className="border d-flex">
-          <img width="150" height="200"alt="film" src={this.props.movie.img} />
+          <img width="150" height="200" alt="film" src={ this.props.movie.img } />
           <div className="flex-fill d-flex flex-column p-3">
-            <h5>{this.props.movie.title}</h5>
-            <hr className='w-100'/>
-            <span>{this.props.movie.details}</span>
+            <h5> { this.props.movie.title } </h5>
+            <hr className="w-100" />
+            <span>{ this.props.movie.details }</span>
           </div>
         </div>
-      </div>
+    </div>
     );
   }
 
